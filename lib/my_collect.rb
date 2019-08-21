3e02996640f1d30207f4e40048ff0625843042f8
Micah Levine <micah.levine@outlook.com>
2019-08-21 01:34:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
-def my_collect(array)
 array = ["Tim Jones", "Tom Smith", "Jim Pagano"]
-collection = []
+
+def my_collect(array)
+  i=0
+name_collection = []
+while i < array.length
   my_collect(array) do |name|
     name.split(" ").first
   end
